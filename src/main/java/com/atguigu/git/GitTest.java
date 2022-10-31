64ce953ad01ab87513f719b313e93ac298c5cfb5
xiaoli <xiaoli@atguigu.com>
2022-10-31 15:07:19 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("Hello git2!");
         System.out.println("Hello git3!");
         System.out.println("Hello git4!");
+        System.out.println("hello-test!");
+        System.out.println("push test!");
     }
 }
