2b5eaf080c7cc22be2efa355e13e632966f35ef5
liijia <117006962+liijia@users.noreply.github.com>
2022-10-31 16:07:44 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,8 @@ public class GitTest {
         System.out.println("Hello git4!");
         System.out.println("hello-test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
+        
+        
     }
 }
