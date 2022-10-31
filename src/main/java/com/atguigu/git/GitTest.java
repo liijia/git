b6069351d1f4371bdc3da142584707a9f9b17f93
xiaoli <xiaoli@atguigu.com>
2022-10-31 10:39:50 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,9 +1,4 @@
 package com.atguigu.git;
-
-/**
- * @author shkstart
- * @create 2022-10-31 10:14
- */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
